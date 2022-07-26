8142f6d7e761aad98c30243b774f7d8ed35be736
bea_pascual <pascuals.beatriz@gmail.com>
2022-07-26 17:06:11 +0200

    Add AddCardorListTitle to manage new task

--- src/components/AddCardorList.js
+++ src/components/AddCardorList.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
-import { Collapse, Fade, Paper, Typography } from '@mui/material'
-import { makeStyles } from '@mui/styles'
+import React, { useState } from 'react'; // eslint-disable-next-line 
+import { Collapse, Fade, Paper, Typography } from '@mui/material';
+ // eslint-disable-next-line 
+import { makeStyles } from '@mui/styles';
+import AddCardorListTitle from './AddCardorListTitle'
 
 
 const AddCardorList = () => {
-  const [open, setOpen] = useState(false);
+   // eslint-disable-next-line 
+  const [open, setOpen] = useState(true);
   return (
     <div>AddCardorList
     <Collapse in={open}>
-      <p>Se ejecuta cuando es open</p>
+      <AddCardorListTitle/>
     </Collapse>
     <Collapse in={!open}>
       se ejecuta cuando NO es open
---
+++ src/components/AddCardorListTitle.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddCardorListTitle = () => {
+  return (
+    <div>AddCardorListTitle
+      <form>
+        <label>
+          <input type="text" name="name" placeholder=' Enter a title for this card'/> 
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default AddCardorListTitle
